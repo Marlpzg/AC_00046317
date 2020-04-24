8e3a70abc87142f20b60f0f3d3c43c5e7313228d
Mario <00046317@uca.edu.sv>
2020-04-23 21:03:27 -0600

    Tarea 2 Versión 1

---
+++ tarea_2_00046317.asm
@@ -0,0 +1,63 @@
+        org     100h
+
+        ;Ejercicio 1
+        ;carné 00046317 -> 4+6+3+1+7
+        mov     ax, 0000h
+        add     ax, 4d
+        add     ax, 6d
+        add     ax, 3d
+        add     ax, 1d
+        add     ax, 7d
+        mov     cl, 5d
+        div     cl
+
+        ;El resultado del promedio es 4 -> "Me recupero"
+        mov     cl, "M"
+        mov     [200h], cl
+        mov     cl, "e"
+        mov     [201h], cl
+        mov     [204h], cl
+        mov     [208h], cl
+        mov     cl, " "
+        mov     [202h], cl
+        mov     cl, "r"
+        mov     [203h], cl
+        mov     [209h], cl
+        mov     cl, "c"
+        mov     [205h], cl
+        mov     cl, "u"
+        mov     [206h], cl
+        mov     cl, "p"
+        mov     [207h], cl
+        mov     cl, "o"
+        mov     [20Ah], cl
+
+        ;Ejercicio 2
+
+        mov     ax, 0000h
+        mov     al, 1d
+        mov     bx, 210h
+        mov     cl, 2d
+est:    mul     cl
+        mov     [bx], al
+        add     bx, 1h
+        cmp     bx, 21Fh
+        jb      est
+
+        ;Ejercicio 3
+        mov     al, 1d
+        mov     [220h], al  ; F0 = 1
+        mov     [221h], al  ; F1 = 1
+        mov     bx, 222h
+fibo:   mov     cx, bx
+        sub     bx, 2h
+        mov     al, [bx]
+        add     bx, 1h
+        add     al, [bx]
+        add     bx, 1h
+        mov     [bx], al
+        add     bx, 1h
+        cmp     bx, 22Fh
+        jb      fibo
+
+        int 10h
